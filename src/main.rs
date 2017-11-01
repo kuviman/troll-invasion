fe28363b9dc0c24a4bfded47c60a2b501e3795e8
kuviman <kuviman@gmail.com>
2017-11-01 18:54:28 +0400

    Basic chat server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+extern crate ws;
+
+const PORT: u16 = 8008;
+
 fn main() {
-    println!("Hello, world!");
+    std::thread::spawn(|| {
+        use std::cell::RefCell;
+        let connections: RefCell<Vec<ws::Sender>> = RefCell::new(Vec::new());
+        ws::listen(("0.0.0.0", PORT), |connection| {
+            connections.borrow_mut().push(connection);
+            |message: ws::Message| {
+                for connection in connections.borrow().iter() {
+                    connection.send(message.clone()).unwrap();
+                }
+                Ok(())
+            }
+        }).unwrap();
+    });
+
+    println!("nick: ");
+    let mut nick = String::new();
+    std::io::stdin().read_line(&mut nick).unwrap();
+    let nick = nick.trim();
+    ws::connect(format!("ws://play.kuviman.com:{}", PORT), |connection| {
+        connection.send(format!("- {} connected", nick)).unwrap();
+        std::thread::spawn(move || {
+            loop {
+                let mut line = String::new();
+                std::io::stdin().read_line(&mut line).unwrap();
+                connection.send(line).unwrap();
+            }
+        });
+        |message| {
+            println!("Received: {}", message);
+            Ok(())
+        }
+    }).unwrap();
 }
