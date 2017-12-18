e54ba2cd96d881c0ea93d3f5f08e372442ac5393
kuviman <kuviman@gmail.com>
2017-12-19 01:52:11 +0400

    Fix build script for windows

--- build.rs
+++ build.rs
@@ -2,6 +2,9 @@ use std::process::Command;
 use std::path::Path;
 
 fn main() {
-    assert!(Command::new("mvn").arg("package").current_dir("server-src").status().unwrap().success());
+    #[cfg(not(windows))]
+        assert!(Command::new("mvn").arg("package").current_dir("server-src").status().unwrap().success());
+    #[cfg(windows)]
+        assert!(Command::new("cmd").arg("/C").arg("mvn.cmd").arg("package").current_dir("server-src").status().unwrap().success());
     std::fs::copy(Path::new("server-src").join("target").join("troll-invasion.jar"), Path::new("target").join("troll-invasion.jar")).unwrap();
 }
