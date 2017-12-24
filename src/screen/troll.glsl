9d22447120ca421ffdbe130788c89f7f2e1be6a3
kuviman <kuviman@gmail.com>
2017-12-24 15:16:52 +0400

    Alpha test

--- src/screen/troll.glsl
+++ src/screen/troll.glsl
@@ -17,5 +17,8 @@ uniform sampler2D u_texture;
 uniform vec4 u_color;
 void main() {
     gl_FragColor = texture2D(u_texture, v_vt) * u_color;
+    if (gl_FragColor.w < 0.5) {
+        discard;
+    }
 }
 #endif
